390910828ee6c7ff4b3b4201f04c5215f9ee94f4
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-09-16 17:04:03 +0500

    .

---
+++ js/my.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded',function() {
+    fetch(`https://cars-pagination.onrender.com/products`)
+    .catch(function(err) {
+        console.log(err);
+        
+    })
+    .finally(function(er) {
+
+        let loader=document.querySelector('.loader')
+        loader.style.display='none'
+        console.log(er);
+    })
+})
